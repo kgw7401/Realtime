4c3e0839f80d0d392e68363c5783ee7f76d904b5
Geonwoo <kgw7401@gmail.com>
2021-08-11 16:13:32 +0900

    백엔드 프론트엔드 셋팅
    django, djangorestframework, django-cors-header mysqlclient 설치
    프론트와 연결

---
+++ backend/my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-tw(22bd=j2jaa)j^fa9s)wek=n_mt70)%01m(w1_3cai#hy!4v'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'Realtime',                  
+        'USER': 'root',                          
+        'PASSWORD': '7401',                
+        'HOST': 'localhost',                   
+        'PORT': '3306',                        
+    }
+}
